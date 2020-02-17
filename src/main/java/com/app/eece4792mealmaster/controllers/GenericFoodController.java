a17e909dfe2a48ad4c212aaf69618175166b8057
Omar Tuffaha <tuffaha.o@husky.neu.edu>
2020-02-17 13:18:38 -0500

    added search by name mapping

--- src/main/java/com/app/eece4792mealmaster/controllers/GenericFoodController.java
+++ src/main/java/com/app/eece4792mealmaster/controllers/GenericFoodController.java
@@ -2,19 +2,21 @@ package com.app.eece4792mealmaster.controllers;
 
 import static com.app.eece4792mealmaster.controllers.Routes.FOOD_API;
 import static com.app.eece4792mealmaster.controllers.Routes.FOOD_ID;
+import static com.app.eece4792mealmaster.controllers.Routes.SEARCH;
+import static com.app.eece4792mealmaster.controllers.Routes.SEARCHTERMS;
 import static com.app.eece4792mealmaster.controllers.Routes.VAR_FOOD_ID;
 
 import com.app.eece4792mealmaster.services.GenericFoodService;
 import com.app.eece4792mealmaster.utils.ApiResponse;
-import com.app.eece4792mealmaster.utils.Utils;
-import javax.servlet.http.HttpSession;
+
+import com.app.eece4792mealmaster.utils.Views;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin(origins = {"*", "http://localhost:3000"}, allowCredentials = "true")
@@ -24,7 +26,15 @@ public class GenericFoodController {
   private GenericFoodService genericFoodService;
 
   @GetMapping(FOOD_API + VAR_FOOD_ID)
+  @JsonView(Views.Detailed.class)
   public ApiResponse getGenericFoodById(@PathVariable(FOOD_ID) Long foodId) {
     return new ApiResponse(genericFoodService.getGenericFoodById(foodId));
   }
+
+  @GetMapping(FOOD_API + SEARCH)
+  @JsonView(Views.Summary.class)
+  public ApiResponse getGenericFoodByName(@RequestParam(SEARCHTERMS) String genericFoodName) {
+    return new ApiResponse(genericFoodService.getGenericFoodByName(genericFoodName));
+  }
+
 }
