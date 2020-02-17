57ee5624a46c8ac3eb0d70ee27e742b10f808a0b
Omar Tuffaha <tuffaha.o@husky.neu.edu>
2020-02-17 13:02:29 -0500

    edited apis

--- src/main/java/com/app/eece4792mealmaster/controllers/GenericFoodController.java
+++ src/main/java/com/app/eece4792mealmaster/controllers/GenericFoodController.java
@@ -2,6 +2,7 @@ package com.app.eece4792mealmaster.controllers;
 
 import static com.app.eece4792mealmaster.controllers.Routes.FOOD_API;
 import static com.app.eece4792mealmaster.controllers.Routes.FOOD_ID;
+import static com.app.eece4792mealmaster.controllers.Routes.VAR_FOOD_ID;
 
 import com.app.eece4792mealmaster.services.GenericFoodService;
 import com.app.eece4792mealmaster.utils.ApiResponse;
@@ -11,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -22,12 +23,8 @@ public class GenericFoodController {
   @Autowired
   private GenericFoodService genericFoodService;
 
-  @GetMapping(FOOD_API + FOOD_ID)
-  public ApiResponse getGenericFoodById(HttpSession session, @RequestParam(FOOD_ID) Long foodId) {
-    Long userId = Utils.getLoggedInUser(session);
-    if (userId == null) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }
+  @GetMapping(FOOD_API + VAR_FOOD_ID)
+  public ApiResponse getGenericFoodById(HttpSession session, @PathVariable(FOOD_ID) Long foodId) {
     return new ApiResponse(genericFoodService.getGenericFoodById(foodId));
   }
 }
